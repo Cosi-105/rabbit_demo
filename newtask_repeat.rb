cdfe84d24932a72c95c32087f38197704c939f82
Pito Salas <pitosalas@gmail.com>
2015-04-10 15:39:55 -0400

    First Commit

---
+++ newtask_repeat.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bunny"
+
+conn = Bunny.new
+conn.start
+
+ch = conn.create_channel
+q = ch.queue("task_queue", :durable => true)
+
+100.times do 
+  |i|
+  msg = "Message #{i}#{"."*rand(10)}"
+  q.publish(msg, :persistent => true)
+  puts " [x] Sent #{msg}"
+end
+sleep 1.0
+conn.close
